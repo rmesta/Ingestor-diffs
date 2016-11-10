6d56e26aea8fa327857851d490d37943270c3fd8
Rick Mesta <rick.mesta@nexenta.com>
2016-11-10 14:46:45 -0800

    [diffs] Merge of Nexenta/Ingestor.git and rmesta/NZA_Ingestor.git
    Signed-off-by: Rick Mesta <rick.mesta@nexenta.com>

--- Ingestor/ingestion-scripts/A0-example.py
+++ Ingestor/ingestion-scripts/A0-example.py
@@ -1,29 +1,43 @@
 #!/usr/bin/env python
-
-# Author: andrew.galloway@nexenta.com
-# Created On: 2013-09-26
-# Last Updated On: 2013-09-26
-# Description:
-#   example python file
-
+#
+# Author:       andrew.galloway@nexenta.com
+# Created On:   2013-09-26
+# Last Updated: 2016-11-10
+# Description:  ingestor python script template
+#
 import sys
-import functions  # local functions.py file
+import os
+from functions import *             # include generic functions file
 
-# name of this script - could be filename, or something unique people will recognize
+#
+# name of this script
+# could be filename, or something unique and recognizable
+#
 script_name = 'A0-example.py'
 
-# put your actual code within this function, be sure to exit 0 if successful and
-# exit 1 if not
+#
+# put your actual code within this function, be
+# sure to exit 0 if successful and exit 1 if not
+#
 def main(bundle_dir): 
 
     if verify_bundle_directory(script_name, bundle_dir):
         # put code here
+        print script_name + ": directory (" + bundle_dir + ") seems valid."
     else:
         print script_name + ": directory (" + bundle_dir + ") not valid."
         sys.exit(1)
 
+#
 # no reason to touch
+#
 if __name__ == '__main__':
+
+    try:
+        os.environ['NXTA_INGESTOR']
+    except KeyError:
+        print "NXTA_INGESTOR var MUST be set !"
+        sys.exit(1)
 
     if len(sys.argv) <= 1:
         print script_name + ": no directory specified."
--- Ingestor/ingestion-scripts/A0-example.sh
+++ Ingestor/ingestion-scripts/A0-example.sh
@@ -1,24 +1,36 @@
 #!/usr/bin/env bash
+#
+# Author:	andrew.galloway@nexenta.com
+# Created On:	2013-09-26
+# Last Updated:	2016-11-09
+# Description:	ingestor bash script template
+#
 
-# Author: andrew.galloway@nexenta.com
-# Created On: 2013-09-26
-# Last Updated On: 2013-09-26
-# Description:
-#   example of an ingestion script to use as a skeleton
-
+#
 # include generic functions file
-source /root/Collector/Ingestor/ingestion-scripts/functions.sh
+#
+source ../.nxrc
+[ -z "${NXTA_INGESTOR}" ] && { echo "NXTA_INGESTOR var MUST be set !"; exit 1; }
+source ${NXTA_INGESTOR}/ingestion-scripts/functions.sh
 
-# name of this script - could be filename, or something unique people will recognize
+#
+# name of this script
+# could be filename, or something unique and recognizable
+#
 SCRIPT_NAME="A0-example.sh"
 
-# put your actual code within this function, be sure to exit 0 if successful and
-# exit 1 if not
+#
+# put your actual code within this function, be
+# sure to exit 0 if successful and exit 1 if not
+#
 main () {
     BUNDLE_DIR=$1 # use BUNDLE_DIR inside here, don't use $1, just for sanity
 }
 
-# this runs first, and does sanity checking before invoking main() function
+#
+# this runs first, and does sanity checking before
+# invoking main() function
+#
 
 # check for necessary directory argument
 if [ -z "$1" ]; then
